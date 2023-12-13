31bda6a16616125e4035dd7f97d0f1db07a037e8
JodiWesso <105722857+JodiWesso@users.noreply.github.com>
2023-12-13 16:32:38 +0200

    Add files
    files added

---
+++ messageGenerator_js/script.js
@@ -0,0 +1,4 @@
+const affirm = ['random_one.html', 'random_two.html', 'random_three.html', 'random_four.html', 
+                'random_five.html','random_six.html', 'random_seven.html'];
+
+function randomAff = ()
